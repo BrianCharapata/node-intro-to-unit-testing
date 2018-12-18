041c1dd59a64412b102d9324900dba423d4f0461
BrianCharapata <bcharapata51@gmail.com>
2018-12-18 11:05:13 -0800

    update

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -8,19 +8,19 @@ const fizzbuzzer = require('../fizzBuzzer');
 describe('module.exports', function() {
 	it('should divide two numbers', function() {
 		const normalCases = [
-			{num: 15, expected: 0}
+			{num: 15, expected: 'fizz-buzz'}
 		];
 		normalCases.forEach(function(input) {
-			const answer =  fizzbuzzer(input.a);
+			const answer =  fizzbuzzer(input.num);
 			expect (answer).to.equal(input.expected);
 		});
 	});
 
 	it('should raise error if args not numbers', function() {
 		const badInputs = [
-			['a', 1],
-			['1', 2],
-			[2, false]
+			['a'],
+			['l'],
+			[false]
 		];
 		badInputs.forEach(function(input) {
 			expect(function() {
@@ -30,22 +30,3 @@ describe('module.exports', function() {
 	});
 });
 
-/*
-module.exports = function(num) {
-	if (typeof num !== 'number') {
-		throw Error('`num` must be a number');
-	}
-	if (num % 15 === 0) {
-		return 'fizz-buzz';
-	}
-	if (num % 5 === 0) {
-		return 'buzz';
-	}
-	if (num % 3 === 0) {
-		return 'fizz';
-	}
-	else {
-		return num;
-	}
-}
-*/
